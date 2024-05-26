65b35776d1e5b751f239498cf058949d0da9ec63
cpalacior <cpalacior@eafit.edu.co>
2024-05-25 19:59:51 -0500

    all methods working

--- backend/methods/chapter3.py
+++ backend/methods/chapter3.py
@@ -6,24 +6,35 @@ from .chapter3_methods import grafica
 
 def calculateLagrange(x, y):
     f = lagrange.main_lagrange(x,y)
+    polin = lagrange.poliToString(list(f))
     #grafica.graficar(f, x, y, 'Interpolacion lagrange')
-    return f
+    return polin
 
 def calculateNewton(x, y):
     coef_0 , coef = newton_inter.newton_inter(x,y) # Vector de coeficientes b --> coef_0 Tabla --> coef
     polin = newton_inter.sym_polinomio(coef_0, x) # Polinomio
-    return coef_0 , coef, polin
+    print(polin)
+    return coef_0 , coef, str(polin.expr)
 
 def calculateSpline(x, y):
     p = spline.spline(x,y,2)
-    list_f = spline.construct_list_polynomials(p) ## --> este está como regular...
+    list_f = spline.construct_list_polynomials(p)
+    i = 0
+    while i < len(list_f):
+        list_f[i] = spline.poliToString(list_f[i].coef)
+        i = i + 1
+    print(list_f)
     #grafica.graficar_spline(list_f,x,y)
-    return p
+    return list_f
 
 def calculateVander(x, y):
     a = vandermonde.vandermonde(x,y)
     pol = vandermonde.np_polinomio(a)
-    #grafica.graficar(pol, x, y, 'Interpolacion Vandermonde')
+    grafica.graficar(pol, x, y, 'Interpolacion Vandermonde')
     polin = vandermonde.pol_to_str(a)
     return polin
 
+def openImage():
+    image = b''
+    with open('../images/foo.png', 'rb') as file:
+        pass
